46e04eea56dc41e6015540db4274dcfefa6c27ab
Axwells <145656137+Axwells@users.noreply.github.com>
2024-10-31 15:38:44 +0100

    skeleton of Groupe and Lecon

--- src/main/java/ch/heigvd/poo/Lecon.java
+++ src/main/java/ch/heigvd/poo/Lecon.java
@@ -1,4 +1,21 @@
 package ch.heigvd.poo;
 
 public class Lecon {
+    private String matiere;
+    private int jourSemaine;
+    private int periodeDebut;
+    private int duree;
+    private String salle;
+
+    public Lecon(String matiere, int jourSemaine, int periodeDebut, int duree, String salle) {
+        this.matiere = matiere;
+        this.jourSemaine = jourSemaine;
+        this.periodeDebut = periodeDebut;
+        this.duree = duree;
+        this.salle = salle;
+    }
+
+    public String horaire(/*args:*/){
+        return "";
+    }
 }
