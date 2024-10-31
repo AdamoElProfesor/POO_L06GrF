2a33010745b7a4c201d85a3de39029ff1905963f
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2024-10-31 15:37:17 +0100

    impl skeleton of Personne Etudiant and Professeur

--- src/main/java/ch/heigvd/poo/personne/Etudiant.java
+++ src/main/java/ch/heigvd/poo/personne/Etudiant.java
@@ -1,4 +1,10 @@
 package ch.heigvd.poo.personne;
 
 public class Etudiant {
+    private int matricule;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/main/java/ch/heigvd/poo/personne/Professeur.java
+++ src/main/java/ch/heigvd/poo/personne/Professeur.java
@@ -1,4 +1,18 @@
 package ch.heigvd.poo.personne;
 
 public class Professeur {
+    private String abreviation;
+
+    public String abreviation() {
+        return abreviation;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String horaire(){
+        return null;
+    }
 }
