c69fd188954200bf5fbdbc7e91821a66f6773e06
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2024-11-11 09:37:20 +0100

    tests in main

--- src/main/java/ch/heigvd/poo/Main.java
+++ src/main/java/ch/heigvd/poo/Main.java
@@ -1,7 +1,42 @@
 package ch.heigvd.poo;
+import ch.heigvd.poo.personne.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+
+        Professeur danielRossier = new Professeur("Daniel", "Rossier", "DRE");
+        Professeur pierDonini = new Professeur("Pier", "Donini", "PDO");
+
+
+        Lecon syeG01 = new Lecon("SYE", 1,1,2,"G01");
+        Lecon sye2A09 = new Lecon("SYE", 4,3,2,"A09");
+        Lecon poo1 = new Lecon("POO", 3,6,2,"H02");
+        Lecon poo2 = new Lecon("POO", 4,6,2,"H02");
+        Lecon poo3 = new Lecon("POO", 4,8,2,"H02");
+        Lecon tic = new Lecon("TIC", 2,1,2,"F06");
+
+        Etudiant johnLennon = new Etudiant("John", "Lennon",1);
+        Etudiant paulMcCartney = new Etudiant("Paul", "Mc Cartney",2);
+        Etudiant ringoStarr = new Etudiant("Ringo", "Starr");
+        Etudiant georgeHarisson = new Etudiant("George", "Harisson");
+        Etudiant rogerWaters = new Etudiant("Roger", "Waters");
+        Etudiant davidGilmour = new Etudiant("David", "Gilmour");
+
+        Groupe il6_1 = new Groupe(1,"IL", 1, johnLennon, paulMcCartney, ringoStarr, georgeHarisson);
+        Groupe si6_1 = new Groupe(2,"SI", 1, rogerWaters, davidGilmour);
+
+        il6_1.definirLecon(syeG01, sye2A09, poo1, poo2, poo3, tic);
+        si6_1.definirLecon(poo1, poo2, poo3);
+
+        Personne[] personnes = {danielRossier, pierDonini, johnLennon, paulMcCartney, ringoStarr, georgeHarisson, rogerWaters, davidGilmour};
+
+        for (int i = 0; i < personnes.length; i++) {
+            System.out.println(personnes);
+        }
+
+        System.out.println(il6_1);
+        System.out.println(pierDonini);*/
+
     }
 }
