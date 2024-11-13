13b21b4eea0af036af64d45e7ed670fcc8ecdedf
Axwells <145656137+Axwells@users.noreply.github.com>
2024-11-13 10:32:53 +0100

    final commit

--- src/main/java/ch/heigvd/poo/Main.java
+++ src/main/java/ch/heigvd/poo/Main.java
@@ -3,40 +3,43 @@ import ch.heigvd.poo.personne.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*
+
 
         Professeur danielRossier = new Professeur("Daniel", "Rossier", "DRE");
         Professeur pierDonini = new Professeur("Pier", "Donini", "PDO");
 
 
-        Lecon syeG01 = new Lecon("SYE", 1,1,2,"G01");
-        Lecon sye2A09 = new Lecon("SYE", 4,3,2,"A09");
-        Lecon poo1 = new Lecon("POO", 3,6,2,"H02");
-        Lecon poo2 = new Lecon("POO", 4,6,2,"H02");
-        Lecon poo3 = new Lecon("POO", 4,8,2,"H02");
+        Lecon syeG01 = new Lecon("SYE", 1,1,2,"G01", danielRossier);
+        Lecon sye2A09 = new Lecon("SYE", 4,3,2,"A09", danielRossier);
+        Lecon poo1 = new Lecon("POO", 3,6,2,"H02", pierDonini);
+        Lecon poo2 = new Lecon("POO", 4,6,2,"H02", pierDonini);
+        Lecon poo3 = new Lecon("POO", 4,8,2,"H02", pierDonini);
         Lecon tic = new Lecon("TIC", 2,1,2,"F06");
 
-        Etudiant johnLennon = new Etudiant("John", "Lennon",1);
-        Etudiant paulMcCartney = new Etudiant("Paul", "Mc Cartney",2);
-        Etudiant ringoStarr = new Etudiant("Ringo", "Starr");
-        Etudiant georgeHarisson = new Etudiant("George", "Harisson");
-        Etudiant rogerWaters = new Etudiant("Roger", "Waters");
-        Etudiant davidGilmour = new Etudiant("David", "Gilmour");
+        danielRossier.setLecons(syeG01, sye2A09);
+        pierDonini.setLecons(poo1, poo2, poo3);
+
+        Etudiant johnLennon = new Etudiant("John", "Lennon",1234);
+        Etudiant paulMcCartney = new Etudiant("Paul", "Mc Cartney",2341);
+        Etudiant ringoStarr = new Etudiant("Ringo", "Starr", 3241);
+        Etudiant georgeHarisson = new Etudiant("George", "Harisson", 4321);
+        Etudiant rogerWaters = new Etudiant("Roger", "Waters", 1324);
+        Etudiant davidGilmour = new Etudiant("David", "Gilmour", 4312);
 
         Groupe il6_1 = new Groupe(1,"IL", 1, johnLennon, paulMcCartney, ringoStarr, georgeHarisson);
         Groupe si6_1 = new Groupe(2,"SI", 1, rogerWaters, davidGilmour);
 
-        il6_1.definirLecon(syeG01, sye2A09, poo1, poo2, poo3, tic);
-        si6_1.definirLecon(poo1, poo2, poo3);
+        il6_1.definirLecons(syeG01, sye2A09, poo1, poo2, poo3, tic);
+        si6_1.definirLecons(poo1, poo2, poo3);
 
         Personne[] personnes = {danielRossier, pierDonini, johnLennon, paulMcCartney, ringoStarr, georgeHarisson, rogerWaters, davidGilmour};
 
         for (int i = 0; i < personnes.length; i++) {
-            System.out.println(personnes);
+            System.out.println(personnes[i]);
         }
 
-        System.out.println(il6_1);
-        System.out.println(pierDonini);*/
+        System.out.println(il6_1.horaire());
+        System.out.println(pierDonini.horaire());
 
     }
 }
